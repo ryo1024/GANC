77daef96231f73cd4c7f13ffc324200f7d94b0a8
Ryo Yamamoto <ryo10244201@berkeley.edu>
2019-07-02 17:07:33 -0700

    some edits on adding noise file

--- img_gaussianNoise/img_gaussianNoise.py
+++ img_gaussianNoise/img_gaussianNoise.py
@@ -1,8 +1,15 @@
 import imageio
 import imgaug as ia
-image = imageio.imread("https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png")
+
 from imgaug import augmenters as iaa
-ia.seed(6)
-gaus = iaa.AdditiveGaussianNoise(scale=(10,80))
-images_aug = gaus.augment_image(image)
-ia.imshow(images_aug)
+import sys
+
+
+
+if __name__ == "__main__":
+    print(sys.argv[1])
+    image = imageio.imread("https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png")
+    ia.seed(6)
+    gaus = iaa.AdditiveGaussianNoise(scale=(10,80))
+    images_aug = gaus.augment_image()
+    ia.imshow(images_aug)
