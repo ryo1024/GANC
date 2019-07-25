4370dac5d5e6f3c74037c1bca4078d0a76de0f01
hiroyuki <hiroyuki-t1161@gmail.com>
2019-07-25 16:33:47 -0700

    ver1.1

--- models/models.py
+++ models/models.py
@@ -1,13 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-!pip install -q tensorflow-gpu==2.0.0-beta1
 import tensorflow as tf
-
 import os
 import time
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
-
 
 OUTPUT_CHANNELS = 3
 def downsample(filters, size, apply_batchnorm=True):
@@ -44,6 +41,129 @@ def upsample(filters, size, apply_dropout=False):
   result.add(tf.keras.layers.ReLU())
 
   return result
+
+
+_URL = 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/facades.tar.gz'
+
+path_to_zip = tf.keras.utils.get_file('facades.tar.gz',
+                                      origin=_URL,
+                                      extract=True)
+
+PATH = os.path.join(os.path.dirname(path_to_zip), 'facades/')
+BUFFER_SIZE = 400
+BATCH_SIZE = 1
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+def load(image_file):
+  image = tf.io.read_file(image_file)
+  image = tf.image.decode_jpeg(image)
+
+  w = tf.shape(image)[1]
+
+  w = w // 2
+  real_image = image[:, :w, :]
+  input_image = image[:, w:, :]
+
+  input_image = tf.cast(input_image, tf.float32)
+  real_image = tf.cast(real_image, tf.float32)
+
+  return input_image, real_image
+
+
+inp, re = load(PATH+'train/100.jpg')
+# casting to int for matplotlib to show the image
+plt.figure()
+print(inp, re)
+print(inp.shape)
+plt.imshow(inp/255.0)
+plt.figure()
+plt.imshow(re/255.0)
+
+
+def resize(input_image, real_image, height, width):
+  input_image = tf.image.resize(input_image, [height, width],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  real_image = tf.image.resize(real_image, [height, width],
+                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+  return input_image, real_image
+
+def random_crop(input_image, real_image):
+  stacked_image = tf.stack([input_image, real_image], axis=0)
+  cropped_image = tf.image.random_crop(
+      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
+
+  return cropped_image[0], cropped_image[1]
+
+# normalizing the images to [-1, 1]
+
+def normalize(input_image, real_image):
+  input_image = (input_image / 127.5) - 1
+  real_image = (real_image / 127.5) - 1
+
+  return input_image, real_image
+
+
+@tf.function()
+def random_jitter(input_image, real_image):
+  # resizing to 286 x 286 x 3
+  input_image, real_image = resize(input_image, real_image, 286, 286)
+
+  # randomly cropping to 256 x 256 x 3
+  input_image, real_image = random_crop(input_image, real_image)
+
+  if tf.random.uniform(()) > 0.5:
+    # random mirroring
+    input_image = tf.image.flip_left_right(input_image)
+    real_image = tf.image.flip_left_right(real_image)
+
+  return input_image, real_image
+
+# As you can see in the images below
+# that they are going through random jittering
+# Random jittering as described in the paper is to
+# 1. Resize an image to bigger height and width
+# 2. Randomnly crop to the original size
+# 3. Randomnly flip the image horizontally
+
+plt.figure(figsize=(6, 6))
+for i in range(4):
+  rj_inp, rj_re = random_jitter(inp, re)
+  plt.subplot(2, 2, i+1)
+  plt.imshow(rj_inp/255.0)
+  plt.axis('off')
+plt.show()
+
+
+def load_image_train(image_file):
+  input_image, real_image = load(image_file)
+  input_image, real_image = random_jitter(input_image, real_image)
+  input_image, real_image = normalize(input_image, real_image)
+
+  return input_image, real_image
+
+def load_image_test(image_file):
+  input_image, real_image = load(image_file)
+  input_image, real_image = resize(input_image, real_image,
+                                   IMG_HEIGHT, IMG_WIDTH)
+  input_image, real_image = normalize(input_image, real_image)
+
+  return input_image, real_image
+
+train_dataset = tf.data.Dataset.list_files(PATH+'train/*.jpg')
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.map(load_image_train,
+                                  num_parallel_calls=tf.data.experimental.AUTOTUNE)
+train_dataset = train_dataset.batch(1)
+
+
+test_dataset = tf.data.Dataset.list_files(PATH+'test/*.jpg')
+# shuffling so that for every epoch a different image is generated
+# to predict and display the progress of our model.
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+test_dataset = test_dataset.map(load_image_test)
+test_dataset = test_dataset.batch(1)
 
 
 def Generator():
@@ -147,15 +267,89 @@ def discriminator_loss(disc_real_output, disc_generated_output):
 
   return total_disc_loss
 
- def generator_loss(disc_generated_output, gen_output, target):
-  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+def generator_loss(disc_generated_output, gen_output, target):
+    gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+    # mean absolute error
+    l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
 
-  # mean absolute error
-  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+    total_gen_loss = gan_loss + (LAMBDA * l1_loss)
 
-  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
-
-  return total_gen_loss
+    return total_gen_loss
 
 generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+
+EPOCHS = 150
+
+def generate_images(model, test_input, tar):
+  # the training=True is intentional here since
+  # we want the batch statistics while running the model
+  # on the test dataset. If we use training=False, we will get
+  # the accumulated statistics learned from the training dataset
+  # (which we don't want)
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15,15))
+
+  display_list = [test_input[0], tar[0], prediction[0]]
+  title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+  for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.title(title[i])
+    # getting the pixel values between [0, 1] to plot it.
+    plt.imshow(display_list[i] * 0.5 + 0.5)
+    plt.axis('off')
+  plt.show()
+
+
+@tf.function
+def train_step(input_image, target):
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    gen_output = generator(input_image, training=True)
+
+    disc_real_output = discriminator([input_image, target], training=True)
+    disc_generated_output = discriminator([input_image, gen_output], training=True)
+
+    gen_loss = generator_loss(disc_generated_output, gen_output, target)
+    disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+  generator_gradients = gen_tape.gradient(gen_loss,
+                                          generator.trainable_variables)
+  discriminator_gradients = disc_tape.gradient(disc_loss,
+                                               discriminator.trainable_variables)
+
+  generator_optimizer.apply_gradients(zip(generator_gradients,
+                                          generator.trainable_variables))
+  discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                              discriminator.trainable_variables))
+
+
+
+def train(dataset, epochs):
+  for epoch in range(epochs):
+    start = time.time()
+
+    for input_image, target in dataset:
+      train_step(input_image, target)
+
+    clear_output(wait=True)
+    for inp, tar in test_dataset.take(1):
+      generate_images(generator, inp, tar)
+
+    # saving (checkpoint) the model every 20 epochs
+    if (epoch + 1) % 20 == 0:
+      checkpoint.save(file_prefix = checkpoint_prefix)
+
+    print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
+                                                        time.time()-start))
+
+train(train_dataset, EPOCHS)
