43beafc320f95c04334d4faf6ffec97fe03a23a8
hiroyuki <hiroyuki-t1161@gmail.com>
2019-08-08 17:42:22 -0700

    completed

--- compile_function/compile_function.py
+++ compile_function/compile_function.py
@@ -1,4 +1,4 @@
-import numpy as np
+import cv2
 from PIL import Image
 import os
 
@@ -7,27 +7,20 @@ N_IMG_DIR = os.path.abspath(os.path.dirname(__file__)) + '/noised_images_cropped
 
 def compile_pictures(image_dir, n_image_dir):
     image_files_compiled = []
-    integer_list1 = []
-    integer_list2 = []
-    for image in image_dir:
-        image_name = os.path.basename(image)
-        img = Image.open(image)
-        name_list1 = list("{}".format(image_name))
-        arrayedimage = np.array(img)
-        for x in name_list1:
-            if type(x) is type(int):
-                integer_list1 += x
-        for n_image in n_image_dir:
-            n_image_name = os.path.basename(n_image)
-            n_img = Image.open(n_image)
-            name_list2 = list("{}".format(n_image_name))
-            arrayedimage2 = np.asarray(n_img)
-            for item in name_list2:
-                if type(item) is type(int):
-                    integer_list2 += item
-            if  integer_list2 == integer_list1:
-                arrayed_images_list = [arrayedimage, arrayedimage2]
-                image_files_compiled += [arrayed_images_list]
+    for root, dirs, files in os.walk(image_dir):
+        files.remove(".DS_Store")
+        for name in sorted(files):
+            print(image_dir + name)
+            img = Image.open(image_dir + name)
+            arrayedimage = cv2.imread(image_dir + name)
+    for n__root, n_dirs, n_files in os.walk(n_image_dir):
+        #n_files.remove(".DS_Store")
+        for n_name in sorted(n_files):
+            n_img = Image.open(n_image_dir + n_name)
+            arrayedimage2 = cv2.imread(n_image_dir + n_name)
+            arrayed_images_list = [arrayedimage, arrayedimage2]
+            image_files_compiled += [arrayed_images_list]
     return image_files_compiled
 
+print(IMG_DIR)
 print(compile_pictures(IMG_DIR, N_IMG_DIR))
